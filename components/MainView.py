ca98d24e81df548e4357c0a2ad68e4fd81f7dbd9
Ragib-Shahriar <ragibshahriar327@gmail.com>
2022-03-31 21:40:17 +0600

    test

--- components/MainView.py
+++ components/MainView.py
@@ -10,12 +10,15 @@ import PIL.Image as Image
 import io
 import numpy as np
 import cv2
-from PySide2.QtGui import QAccessible, QAccessibleEvent, QAccessibleInterface
-from PySide2.QtCore import QObject
-import PySide2.QtWidgets as pyQtWidget
+# from PySide2.QtGui import QAccessible, QAccessibleEvent, QAccessibleInterface
+# from PySide2.QtCore import QObject
+# import PySide2.QtWidgets as pyQtWidget
 
 from components.ImageProcessing import imgProcess
 # import matplotlib.pyplot as plt
+from components.Worker import Worker
+
+from PyQt5.QtCore import QObject, QThread, pyqtSignal
 
 class cMainView(QWidget):
     def __init__(self, mainwindow, vbox_layout):
@@ -77,6 +80,23 @@ class cMainView(QWidget):
     def goto_page(self, page_num):
         # self.mainwindow.cstatus_bar.show_msg('working')
         img = imgProcess.get_single_page(self.path,page_num)
+
+        # Step 2: Create a QThread object
+        # self.thread = QThread()
+        # # Step 3: Create a worker object
+        # self.worker = Worker()
+        # self.worker.init(self,self.vbox_layout,self.scroll_area,self.verticalSpacer,img, self.zoom, self.page_num)
+        # # Step 4: Move worker to the thread
+        # self.worker.moveToThread(self.thread)
+        # # Step 5: Connect signals and slots
+        # self.thread.started.connect(self.worker.run)
+        # self.worker.finished.connect(self.thread.quit)
+        # self.worker.finished.connect(self.worker.deleteLater)
+        # self.thread.finished.connect(self.thread.deleteLater)
+        # # self.worker.progress.connect(self.reportProgress)
+        # # Step 6: Start the thread
+        # self.thread.start()
+
         self.show_single_page(img)
         # self.show_single_page(self.imgs[page_num])
 
@@ -163,12 +183,13 @@ class cMainView(QWidget):
     #     [tempQW, page_text] = self.create_page(self.imgs[0])
     #     print(page_text)
     def debug_accessibility(self):
-        print('accessibility debug', QAccessible.isActive())
-        qobj = QObject()
-        qobj = pyQtWidget.QLabel()
-        qobj.setText('hello')
-        event = QAccessibleEvent(qobj, QAccessible.Focus)
-        QAccessible.updateAccessibility(event)
+        pass
+        # print('accessibility debug', QAccessible.isActive())
+        # qobj = QObject()
+        # qobj = pyQtWidget.QLabel()
+        # qobj.setText('hello')
+        # event = QAccessibleEvent(qobj, QAccessible.Focus)
+        # QAccessible.updateAccessibility(event)
 
     def show_single_page(self, img):
 
@@ -185,7 +206,7 @@ class cMainView(QWidget):
 
         no_page = len(self.imgs)
 
-        [tempQW, pg_text] = self.create_page(img)
+        [tempQW] = self.create_page(img)
         form_layout.addRow(tempQW)
         groupBox.setLayout(form_layout)
 
@@ -223,13 +244,12 @@ class cMainView(QWidget):
 
     def create_page(self, img):
 
+
         container = QWidget()
         container.setStyleSheet('background: white;')
         page_width = QtWidgets.QDesktopWidget().screenGeometry().width()-3*80
         page_height = self.scroll_area.height()
         # page_height =  QtWidgets.QDesktopWidget().screenGeometry().height()
-
-        # container.setFixedHeight(page_height)
 
         # DROP SHADOW EFFECT
         effect = QGraphicsDropShadowEffect()
@@ -240,34 +260,69 @@ class cMainView(QWidget):
         container.setGraphicsEffect(effect)
         label = QLabel(container)
         label.setText("first label")
-        # label.setStyleSheet('border: 3px solid gray')
+       
+        [no_of_lines,pixMap,bounding_box,height_ratio,width_ratio] = self.process(img,page_width,page_height)
 
-        # adding pixmap
-        # img = img.convert("RGB")
-        # data = img.tobytes("raw", "RGB")
+        pixMap_width = pixMap.size().width()
+        pixMap_height = pixMap.size().height()
 
-        # convert to PIL img
-        # image = Image.open(io.BytesIO(img_data))
+        container.setFixedWidth(pixMap_width)
+        container.setFixedHeight(pixMap_height)
 
-        # convert to cv2 img
+        label.setPixmap(pixMap)
+
+        # label creation per line
+
+        # CREATING a HORIZONTAL LAYOUT to contain spacers and the container
+        hbox_temp = QHBoxLayout()
+        hbox_temp.setContentsMargins(0, 0, 0, 0)
+
+        if no_of_lines == 0:            
+            hbox_temp.addItem(self.verticalSpacer)
+            hbox_temp.addWidget(container)
+            hbox_temp.addItem(self.verticalSpacer)
+
+        else:
+            bounding_vertical_rect = []
+            
+            
+            for i, (x1, x2, r1, r2, txt) in enumerate(bounding_box):                
+            
+                line = QLabel(container)
+                
+                line.setText(txt)
+                line.setTextInteractionFlags(Qt.TextSelectableByMouse)
+                line.setStyleSheet("background:white")
+                
+                line.setFont(QFont('Arial',12))
+                line.adjustSize()
+                # print(i)
+                line.move(x1*width_ratio, r1*height_ratio)
+                line.adjustSize()
+                line.setFixedHeight((r2-r1+12)*height_ratio)
+                line.setFixedWidth((x2-x1+150)*width_ratio)
+                line.setFocusPolicy(Qt.FocusPolicy.StrongFocus)
+
+                
+            hbox_temp.addItem(self.verticalSpacer)
+            hbox_temp.addWidget(container)
+
+            hbox_temp.addItem(self.verticalSpacer)
+
+        tempQW = QWidget()
+
+        tempQW.setLayout(hbox_temp)
+
+        return [tempQW]
+
+    def process(self,img,page_width,page_height):       
         cvImage = cv2.cvtColor(np.array(img), cv2.COLOR_RGB2BGR)
-
-
-
         # convert to gray img
         gray_img = imgProcess.bgr2gray(cvImage)
-
-        # plt.imshow(gray_img, cmap='gray')
-
         # convert to invert image
         inv_img = imgProcess.invertImage(gray_img)
-        # plt.imshow(inv_img)
-
         # convert to horizontal histogram
         [hist_img, hist_data] = imgProcess.horizontal_hist(inv_img)
-
-        # plt.imshow(hist_img)
-
         # get horizontal rounding_rect
         bounding_horizontal_rect = imgProcess.bounding_horizontal_rect(
             hist_data)
@@ -278,29 +333,6 @@ class cMainView(QWidget):
         # cropped img
         lines = imgProcess.find_lines(bounding_horizontal_rect, inv_img)
 
-        # plt.imshow(lines[0],cmap='gray')
-        # plt.imshow(lines[1],cmap='gray')
-        # plt.show()
-        cv2.imwrite('line-1.png', lines[0])
-        cv2.imwrite('line-2.png', lines[1])
-        # print(len(lines))
-
-
-        # vertical histogram applied on each line
-        # line_vert_hist_data = []
-        # bounding_vertical_rect = []
-        # for i in range(no_of_lines):
-
-        #     [vert_img, vert_data] = imgProcess.vertical_hist(lines[i])
-        #     # line_vert_hist_data.append(vert_data)
-
-        # # find bounding_verting_rect
-        #     words_in_line = imgProcess.bounding_vertical_rect(vert_data)
-        #     bounding_vertical_rect.append(words_in_line)
-
-
-        # hist_img_rgb = cv2.cvtColor(hist_img, cv2.COLOR_GRAY2RGB)
-
         img_bytes = img.tobytes()
 
         qim = QImage(img_bytes, img.size[0],
@@ -310,33 +342,22 @@ class cMainView(QWidget):
 
         pixMap = pixMap.scaled(page_width,
                                page_height*self.zoom, Qt.KeepAspectRatio)
-        label.setGeometry(QRect(0, 0, pixMap.width(), pixMap.height()))
-
-        self.pixMap_width = pixMap.size().width()
-        self.pixMap_height = pixMap.size().height()
-
-        container.setFixedWidth(self.pixMap_width)
-        container.setFixedHeight(self.pixMap_height)
-
-        label.setPixmap(pixMap)
+        
+        pixMap_width = pixMap.size().width()
+        pixMap_height = pixMap.size().height()
 
         # label creation per line
 
         img_height = img.height
         img_width = img.width
-        height_ratio = self.pixMap_height/img_height
-        width_ratio = self.pixMap_width/img_width
+        height_ratio = pixMap_height/img_height
+        width_ratio = pixMap_width/img_width
 
         # CREATING a HORIZONTAL LAYOUT to contain spacers and the container
-        hbox_temp = QHBoxLayout()
-        hbox_temp.setContentsMargins(0, 0, 0, 0)
         page_text = []
-
+        bounding_box = []
         if no_of_lines == 0:            
-            hbox_temp.addItem(self.verticalSpacer)
-            hbox_temp.addWidget(container)
-            hbox_temp.addItem(self.verticalSpacer)
-
+            return [no_of_lines,pixMap,bounding_box,height_ratio,width_ratio]
         else:
             bounding_vertical_rect = []
             
@@ -365,49 +386,11 @@ class cMainView(QWidget):
                 # print(txt)
                 if len(txt) == 0 :
                     txt = imgProcess.pytesseract_apply(lines[i],flag=1, line=i)
-                    # print(i+1)
-                    # words = imgProcess.find_words(words_in_line,
-                    #                             lines[i])
-                    # for word in words :
-                    #     # plt.axis('off')
-                    #     # plt.imshow(word, cmap='gray')
-                    #     # plt.figure(i+1)
-                    #     word_found = imgProcess.pytesseract_apply(lines[i],flag=1 )
-                        # txt += str(word_found + " ")
-
-
-                # save txt to txt file 
-                # with open('sample.txt', 'a') as f:
-                #     if not i==1: f.write('\n')
-                #     f.write(txt)
                 page_text.append(txt)
 
                 x1 = bounding_vertical_rect[i][0][0]
                 x2 = bounding_vertical_rect[i][-1][1]
                 
-                line = QLabel(container)
+                bounding_box.append((x1,x2,r1,r2,txt))
                 
-                line.setText(txt)
-                line.setTextInteractionFlags(Qt.TextSelectableByMouse)
-                line.setStyleSheet("background:white")
-                
-                line.setFont(QFont('Arial',12))
-                line.adjustSize()
-                # print(i)
-                line.move(x1*width_ratio, r1*height_ratio)
-                line.adjustSize()
-                line.setFixedHeight((r2-r1+12)*height_ratio)
-                line.setFixedWidth((x2-x1+150)*width_ratio)
-                line.setFocusPolicy(Qt.FocusPolicy.StrongFocus)
-
-                
-            hbox_temp.addItem(self.verticalSpacer)
-            hbox_temp.addWidget(container)
-
-            hbox_temp.addItem(self.verticalSpacer)
-
-        tempQW = QWidget(self)
-
-        tempQW.setLayout(hbox_temp)
-
-        return [tempQW, page_text]
+        return [no_of_lines,pixMap,bounding_box,height_ratio,width_ratio]
