83fc49d87e78d8ce9fcf2fc5304c3afcca7c9616
Ragib-Shahriar <ragibshahriar327@gmail.com>
2022-04-10 22:59:41 +0600

    commit test

--- run.py
+++ run.py
@@ -1,7 +1,8 @@
+from curses import ACS_DARROW
 import sys
 from PyQt5.QtWidgets import QApplication
 from GUIMain import GUIMainWindow
-
+# ACS_DARROW
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     win = GUIMainWindow()
