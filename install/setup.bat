7596004958f99dbd657ebbbcb3a2f48a0a2800da
Ragib-Shahriar <35685748+Ragib-Shahriar@users.noreply.github.com>
2022-06-03 21:48:15 +0600

    Delete install directory

--- install/setup.bat
+++
@@ -1,9 +0,0 @@
-rem @echo off
-rem @setlocal enableextensions
-rem @cd /d "%~dp0"
-if not exist "C:\Program Files\Tesseract-OCR\" (
-  call tesseract-ocr-w64-setup-v5.0.1.20220118.exe 
-  xcopy /E %cd%\ben.traineddata "C:\Program Files\Tesseract-OCR\tessdata"
-)
-call pathak-installer.exe
-pause
